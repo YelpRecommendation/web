f7e9085f1d5f54af62365f517d301f6d6d5fc0c5
Yuexi Ma <yuexi@seas.upenn.edu>
2015-04-14 15:53:52 -0400

    http working

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,15 @@ Rails.application.routes.draw do
   
   get 'signup' => 'signup#new'
   get 'signin' => 'sessions#new'
+  get 'signout' => 'sessions#destroy'
+
+
+  
+  
   get 'recom' => 'recom#home'
   get 'wait' => 'wait#home'
-  
 
+  get 'sessions' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
