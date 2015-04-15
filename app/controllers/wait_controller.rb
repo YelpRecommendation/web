d0989c9b90b1ce40921e55c958d64e4de9ab4bac
SunshineCao <yangcao@seas.upenn.edu>
2015-04-14 20:45:21 -0400

    wait need to be modified

--- app/controllers/wait_controller.rb
+++ app/controllers/wait_controller.rb
@@ -1,25 +1,26 @@
 class WaitController < ApplicationController
   def home
-      require 'socket'
+      #require 'socket'
       
-      host = '192.168.64.1'     # The web server
-      port = 8078
-      path = "/control"                 # The file we want
+      #host = '192.168.64.1'     # The web server
+      #port = 8078
+      #path = "/control"                 # The file we want
       
-      cate = params[:value]
+      #cate = params[:value]
+      #name = cookies[:username]
       # This is the HTTP request we send to fetch a file
-      request = "POST #{path} HTTP/1.0\r\n\r\nusername=&content="
+      #request = "POST #{path} HTTP/1.0\r\n\r\nusername="+name+"&content="
       
-      socket = TCPSocket.open(host,port)  # Connect to server
-      socket.print(request)               # Send request
-      response = socket.read              # Read complete response
+      #socket = TCPSocket.open(host,port)  # Connect to server
+      #socket.print(request)               # Send request
+      #response = socket.read              # Read complete response
       
-      print response
+      #print response
       
       # Split response at first blank line into headers and body
-      headers,body = response.split("\r\n\r\n", 2)
-      print body                          # And display it
+      #headers,body = response.split("\r\n\r\n", 2)
+      #print body                          # And display it
       
-      
+      redirect_to "/search"
   end
 end
