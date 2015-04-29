4c19e739c9c92622be3891f4f14eea445dfb1d85
SunshineCao <yangcao@seas.upenn.edu>
2015-04-29 19:20:53 -0400

    add myaccount

---
+++ app/controllers/my_account_controller.rb
@@ -0,0 +1,4 @@
+class MyAccountController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  get 'my_account/home'
+
   get 'review/home'
 
   get 'sessions/new'
